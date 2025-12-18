4609f87d3d71bef547d7b1be12c20b410b3e61d5
zcroft27 <croft.z@northeastern.edu>
2025-12-17 21:40:21 -0500

    added explanation and capture of global variable

--- interrupts/sig.py
+++ interrupts/sig.py
@@ -6,12 +6,61 @@ count = 0
 def handler(sig, frame):
     global count
     count += 1
-    print(f"START handler #{count} at {time.time():.1f}")
+    n = count
+    print(f"START handler #{n} at {time.time():.1f}")
     time.sleep(3)
-    print(f"END handler #{count} at {time.time():.1f}")
+    print(f"END handler #{n} at {time.time():.1f}")
 
 signal.signal(signal.SIGINT, handler)
 
-print("Press Ctrl+C multiple times rapidly!")
+print("Press Ctrl+C!")
 time.sleep(5)
 print(f"Total handler runs: {count}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Even though Python is single-threaded, a signal can interrupt a blocking call inside a signal handler,
+# causing another handler invocation to start before the previous one finishes.
